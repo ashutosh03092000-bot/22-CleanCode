65ecbc6a4d3ed5b602ba9fe1985b0525e5d0506f
Ashutosh Kumar Sahu <75112792+ashutosh03092000-bot@users.noreply.github.com>
2023-11-22 11:46:52 +0530

    Create Day1_M_Variables.cs

---
+++ Day1_M_Variables.cs
@@ -0,0 +1,137 @@
+review 1
+public bool IsShopOpen(string day)
+{
+    if (!string.IsNullOrEmpty(day))
+    {
+        day = day.ToLower();
+        if (day == "friday")
+        {
+            return true;
+        }
+        else if (day == "saturday")
+        {
+            return true;
+        }
+        else if (day == "sunday")
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    else
+    {
+        return false;
+    }
+
+}
+
+//Avoid nesting too deeply
+public bool IsShopOpen(string day)
+{
+    if (string.IsNullOrEmpty(day))
+    {
+        return false;
+    }
+    string[] openingDays = new[] { "friday", "saturday", "sunday" };
+    return openingDays.Any(d => d == day.ToLower());
+}
+
+
+review 3
+public bool IsShopOpen(string day)
+{
+    if (!string.IsNullOrEmpty(day))
+    {
+        day = day.ToLower();
+        if (day == "friday")
+        {
+            return true;
+        }
+        else if (day == "saturday")
+        {
+            return true;
+        }
+        else if (day == "sunday")
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    else
+    {
+        return false;
+    }
+
+}
+
+//Avoid nesting too deeply
+public bool IsShopOpen(string day)
+{
+    if (string.IsNullOrEmpty(day))
+    {
+        return false;
+    }
+    string[] openingDays = new[] { "friday", "saturday", "sunday" };
+    return openingDays.Any(d => d == day.ToLower());
+}
+review 4
+  
+//Avoid naming variables that cant be understand
+var l = new[] { "Austin", "New York", "San Francisco" };
+
+for (var i = 0; i < l.Count(); i++)
+{
+    var li = l[i];
+    DoStuff();
+    DoSomeOtherStuff();
+
+    // ...
+    // ...
+    // ...
+    // Wait, what is `li` for again?
+    Dispatch(li);
+}
+
+review 5
+  if (userRole == 8) // Check if Admin 
+{
+    
+}
+//Avoid magic numbers, dont hard code 
+
+review 6
+public class Car
+{
+    public string CarMake { get; set; }
+    public string CarModel { get; set; }
+    public string CarColor { get; set; }
+
+    //...
+}
+
+//Redundant, unecessary content is present
+
+review 7
+var ymdstr = DateTime.UtcNow.ToString("MMMM dd, yyyy");
+//Variable naming should be clear
+
+review 9
+public void CreateMicrobrewery(string name = null)
+{
+    var breweryName = !string.IsNullOrEmpty(name) ? name : "Hipster Brew Co.";
+    // ...
+}
+
+
+//Use Simplicity
+public void CreateMicrobrewery(string name = null)
+{
+    var breweryName = name?? "Hipster Brew Co.";
+    // ...
+}
